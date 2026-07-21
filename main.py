a62e03fe5e281c89e4599bfb68a8bfa8ef885d2b
nvssrkameswar <nvssrkameswar@gmail.com>
2026-07-21 17:06:09 +0530

    react agent using langchain-tavily

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
-from typing import Any
 from dotenv import load_dotenv
 from langchain.agents import create_agent
-from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
 load_dotenv()
 
-tavily = TavilyClient()
-
-@tool
-def search(query: str) -> Any:
-    """
-    Tool that searches the query over the internet.
-    Args:
-        query: The query to search for.
-    Returns:
-        The search results.
-    """
-    print(f"Searching for: {query}")
-    return tavily.search(query=query)
-
 llm = ChatOllama(model="gpt-oss", base_url="http://localhost:11434")
-tools = [search]
+tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 
 def main():
